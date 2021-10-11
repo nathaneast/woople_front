c29341e1f4f9e024b845d298707623bc19de7d44
nathaneast <ahrl_ahrl@naver.com>
2021-10-11 18:57:27 +0900

    작업중: Header 레이아웃

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -4,13 +4,15 @@ interface Props {
 
 const Header = ({ toggleItemForm }: Props) => {
   return (
-    <header>
-      <div>
-        <h1>WOOPLE</h1>
+    <header className="flex justify-center items-center">
+      <div className="flex justify-center items-center">
+        <div>
+          <h1 className="p-4 text-4xl">WOOPLE</h1>
+        </div>
+        <ul>
+          <li onClick={toggleItemForm}>글쓰기</li>
+        </ul>
       </div>
-      <ul>
-        <li onClick={toggleItemForm}>글쓰기</li>
-      </ul>
     </header>
   );
 };
