4edfac5c46b0c5658fe914cdf7a77866c807bd12
nathaneast <ahrl_ahrl@naver.com>
2021-10-11 18:25:09 +0900

    완료: tailwind 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
